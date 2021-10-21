f0c508da998e0262f136879ba15c014329e4cb08
Obez99 <giuliogx@live.it>
2021-10-21 17:13:27 +0200

    Adds card add function

--- script.js
+++ script.js
@@ -39,30 +39,89 @@ const members =
 
 const teamContainer = document.querySelector(".team-container");
 
-for (let key in members) {
-  const card = document.createElement("div")
-  card.classList.add("team-card")
-  teamContainer.append(card)
+addCards();
 
-  const cardImage = document.createElement("div")
-  cardImage.classList.add("card-image")
-  card.append(cardImage)
+addCard();
 
-  const cardText = document.createElement("div")
-  cardText.classList.add("card-text")
-  card.append(cardText)
 
-  const cardImageTag = document.createElement("img")
-  cardImageTag.src = members[key]["image"]
-  cardImageTag.alt = members[key]["name"]
-  cardImage.append(cardImageTag)
 
-  const cardTitle = document.createElement("h3")
-  cardTitle.innerHTML += members[key]["name"]
-  cardText.append(cardTitle)
 
-  const cardRole = document.createElement("p")
-  cardRole.innerHTML += members[key]["role"]
-  cardText.append(cardRole)
+function addCards() {
 
+  for (let key in members) {
+    const card = document.createElement("div")
+    card.classList.add("team-card")
+    teamContainer.append(card)
+
+    const cardImage = document.createElement("div")
+    cardImage.classList.add("card-image")
+    card.append(cardImage)
+
+    const cardText = document.createElement("div")
+    cardText.classList.add("card-text")
+    card.append(cardText)
+
+    const cardImageTag = document.createElement("img")
+    cardImageTag.src = members[key]["image"]
+    cardImageTag.alt = members[key]["name"]
+    cardImage.append(cardImageTag)
+
+    const cardTitle = document.createElement("h3")
+    cardTitle.innerHTML += members[key]["name"]
+    cardText.append(cardTitle)
+
+    const cardRole = document.createElement("p")
+    cardRole.innerHTML += members[key]["role"]
+    cardText.append(cardRole)
+
+  }
+}
+
+function addCard() {
+  const addButton = document.getElementById("addMemberButton")
+  const formName = document.getElementById("name")
+  const formRole = document.getElementById("role")
+  const formImage = document.getElementById("image")
+
+
+  addButton.addEventListener("click", function () {
+    console.log(formName.value);
+    console.log(formRole.value);
+    console.log(formImage.value);
+
+    const newCard = {
+      name: formName.value,
+      role: formRole.value,
+      image: formImage.value
+    }
+
+    const card = document.createElement("div")
+    card.classList.add("team-card")
+    teamContainer.append(card)
+
+    const cardImage = document.createElement("div")
+    cardImage.classList.add("card-image")
+    card.append(cardImage)
+
+    const cardText = document.createElement("div")
+    cardText.classList.add("card-text")
+    card.append(cardText)
+
+    const cardImageTag = document.createElement("img")
+    cardImageTag.src = formImage.value
+    cardImageTag.alt = formName.value
+    cardImage.append(cardImageTag)
+
+    const cardTitle = document.createElement("h3")
+    cardTitle.innerHTML += formName.value
+    cardText.append(cardTitle)
+
+    const cardRole = document.createElement("p")
+    cardRole.innerHTML += formRole.value
+    cardText.append(cardRole)
+
+    members.push(newCard)
+
+
+  })
 }
